501ce869be35b79d26cfd25ad423c9c803031a21
asukim2020 <63150458+asukim2020@users.noreply.github.com>
2021-07-15 17:53:33 +0900

    HttpServlet 을 통한 http 통신

---
+++ servlet/src/main/java/hello/servlet/basic/HelloServlet.java
@@ -0,0 +1,35 @@
+package hello.servlet.basic;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void service(
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) throws ServletException, IOException {
+        // 서블릿 시작시 호출
+        System.out.println("HelloServlet.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+
+        // header 정보
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+
+        // body 정보
+        response.getWriter().write("hello " + username);
+
+    }
+
+}
